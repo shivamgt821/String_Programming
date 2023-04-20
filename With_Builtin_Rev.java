351b39c739c27c5fdb47392c941cfcfd37e3b250
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-20 10:33:09 +0530

    Create With_Builtin_Rev.java

---
+++ With_Builtin_Rev.java
@@ -0,0 +1,26 @@
+package com.jsp.Advancejava;
+
+public class With_Builtin_Rev {
+
+	public static void main(String[] args) {
+		String s1 = "java is an oop lang";// avaj si na poo gnal
+		String s2 = "";
+		String [] arr = s1.split(" ");
+		
+		for(String s:arr) {
+			s2=s2+rev(s);
+			s2=s2+" ";
+		}
+		s2=s2.trim();
+		System.out.println(s2);
+	}
+	
+	static String rev(String s) {
+		String rev ="";
+		for(int i=s.length()-1;i>=0;i--) {
+			rev+=s.charAt(i);
+		}
+		return rev;
+	}
+}
+
