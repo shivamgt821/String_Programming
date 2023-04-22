872853c6d4262cac30c160f5ce949b0bee1b0394
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-22 19:29:17 +0530

    Create String_trim.java

---
+++ String_trim.java
@@ -0,0 +1,18 @@
+public class String_trim {
+
+	public static void main(String[] args) {
+
+		String s = " java developer ";
+		System.out.println(s.trim());
+		
+		String s1 = "developer";
+		System.out.println(s1.indexOf('e'));
+		System.out.println(s1.indexOf("lop"));
+		System.out.println(s1.indexOf('e' , 2));
+		System.out.println(s1.indexOf('e' , 5));
+		System.out.println(s1.indexOf('x'));
+		
+		
+	}
+
+}
