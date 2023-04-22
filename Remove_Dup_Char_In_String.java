6fec52a6fd89c52fcbf3bc77e193209f9399c938
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-22 19:25:27 +0530

    Create Remove_Dup_Char_In_String.java

---
+++ Remove_Dup_Char_In_String.java
@@ -0,0 +1,19 @@
+package com.jsp.Advancejava;
+
+public class Remove_Dup_Char_In_String {
+
+	public static void main(String[] args) {
+		String s1="abbbccddffeeegg";
+		
+		String s2 = "";
+		
+		while(s1.length()>0){
+			
+			char c1 = s1.charAt(0);
+			s1 = s1.replace(c1+"", "");
+			s2 = s2+c1;
+		}
+		System.out.println(s2);
+	}
+
+}
