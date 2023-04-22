6629d139d9d9e5aa1cd45957e5997e22d22552ea
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-22 19:28:03 +0530

    Create Split_String.java

---
+++ Split_String.java
@@ -0,0 +1,14 @@
+public class Split_String {
+
+	public static void main(String[] args) {
+		
+		String s1 = "java is an oop lang";
+//		String [] arr = s1.split("");
+		String [] arr = s1.split("a"); 
+		
+		
+	for(String s : arr) {
+			
+			System.out.print(s)
+      }
+ }
