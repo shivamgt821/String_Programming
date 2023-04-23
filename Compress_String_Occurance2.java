cc4c6ccdc938991bd9e0f048ab91b9ccea9c9515
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-04-23 11:32:11 +0530

    Create Compress_String_Occurance2.java
    INPUT--> aaabbbcc
    
    OUTPUT-->a3b3c2

---
+++ Compress_String_Occurance2.java
@@ -0,0 +1,26 @@
+import java.util.LinkedHashMap; 
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Compress_String_Occurance2 {
+
+	public static void main(String[] args) {
+		String str = "aaabbbcc";
+		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+		for(int i=0; i<str.length(); i++) {
+			
+			if(map.get(str.charAt(i)) != null){
+				map.put(str.charAt(i), map.get(str.charAt(i))+1);
+			}
+			else {
+				map.put(str.charAt(i), 1);
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for(Entry<Character, Integer> entry : map.entrySet()) {
+			sb.append(entry.getKey());
+			sb.append(entry.getValue());
+		}  
+		 System.out.println(sb);
+	}
+}
